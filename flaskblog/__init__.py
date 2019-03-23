63418a126afcb710ac0903d2ee7f212eb7806e51
chaluvadi1 <venkatamanojgupta@gmail.com>
2019-03-23 11:55:49 -0400

    Added new account page and added conditions for nav bar

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,10 +4,12 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'AnyRandomStringOFcHaracters'
+app.config['SECRET_KEY'] = '$2b$12$MbmOE0OEARCESk8o0kuwkOl6a3.dkvk/u9LQaFue2g1A2.MFlroFW'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
