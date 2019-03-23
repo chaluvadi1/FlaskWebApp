5ec55b543d2285adb1fdc1811879db308a7fabee
chaluvadi1 <venkatamanojgupta@gmail.com>
2019-03-23 11:21:32 -0400

    Reformatted the code to pep8 compliance and formatted html code

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,34 +1,35 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField
-from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
+
 class RegistrationForm(FlaskForm):
-	username = StringField('Username', 
-			validators=[DataRequired(),Length(min=5,max=20)])
-	email = StringField('Email',
-			validators=[DataRequired(),Email()])
-	password = PasswordField('Password',
-			validators=[DataRequired(),Length(min=8,max=50)])
-	confirm_password = PasswordField('Confirm Password',
-			validators=[DataRequired(),Length(min=8,max=50),EqualTo('password')])
-	submit = SubmitField('Sign Up')
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=5, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=8, max=50)])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), Length(min=8, max=50), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
-	def validate_username(self, username):
-	    user = User.query.filter_by(username=username.data).first()
-	    if user:
-		    raise ValidationError('Username already exists please choose a new user name')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already exists please choose a new user name')
 
-	def validate_email(self, email):
-	     user = User.query.filter_by(email=email.data).first()
-	     if user:
-		     raise ValidationError('Email already exists please choose a new email')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already exists please choose a new email')
 
 
 class LoginForm(FlaskForm):
-	email = StringField('Email',
-			validators=[DataRequired(),Email()])
-	password = PasswordField('Password',
-			validators=[DataRequired(),Length(min=8,max=50)])
-	remember = BooleanField('Remember Me')
-	submit = SubmitField('Login')	
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=8, max=50)])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
